27eb310edb3b782c1cf061064863ec85052ed241
shahid_nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-23 10:11:16 +0500

    calculate total with formula

--- boolVsCopm.js
+++ boolVsCopm.js
@@ -1,10 +1,13 @@
-let i =0;
-let n=0;
-while(i<=10){
-    n=n+i;
-    console.log(`the value of i is : ${i}`);
-    i++;
+let i =10;
+// let n=0;
+// while(i<=10){
+//     n=n+i;
+//     console.log(`the value of i is : ${i}`);
+//     i++;
     
-}
-console.log("the value of i is : "+ i)
-console.log(`toatal sum = ${n}`)
+// }
+// console.log("the value of i is : "+ i)
+// console.log(`toatal sum = ${n}`)
+
+let toatal = i*(i+1)/2
+console.log(toatal)
